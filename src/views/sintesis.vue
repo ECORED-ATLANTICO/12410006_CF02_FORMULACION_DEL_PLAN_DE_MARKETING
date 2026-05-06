f0d17bfd992999659c565cd861d3a069a2ee5d02
AdonadoMolinares <donadomolinares@gmail.com>
2026-05-05 23:47:32 -0500

    commit

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,11 +2,11 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum.
+    p El componente formativo Diseño y control del plan de <em>marketing</em>, aborda la formulación de objetivos, estrategias y tácticas como base para la estructuración del plan, integrando la planificación operativa mediante presupuesto y cronograma. Asimismo, incorpora los índices de gestión como herramientas de control, junto con la auditoría de <em>marketing</em> y la mejora continua, permitiendo evaluar el desempeño, optimizar las decisiones comerciales y garantizar coherencia entre la planificación, ejecución y resultados organizacionales.
     .row.justify-content-center
-      .col-lg-10.mb-5.bgs.p-4.brad
+      .col-lg-12.mb-5.bgs.p-4.brad
         figure
-          img(src="@/assets/curso/sintesis.svg", alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum. ")
+          img(src="@/assets/curso/sintesis.svg", alt="El componente formativo Diseño y control del plan de marketing aborda la formulación de objetivos, estrategias y tácticas como base para la estructuración del plan, integrando la planificación operativa mediante presupuesto y cronograma. Asimismo, incorpora los índices de gestión como herramientas de control, junto con la auditoría de marketing y la mejora continua, permitiendo evaluar el desempeño, optimizar las decisiones comerciales y garantizar coherencia entre la planificación, ejecución y resultados organizacionales.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
